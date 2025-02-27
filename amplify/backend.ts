8fdf1e821c45fb2fd75ed0cfc8100c6b4dca3b51
clover4869 <153183368+clover4869@users.noreply.github.com>
2025-02-27 10:18:51 +0000

    feat

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,12 +2,12 @@ import { defineBackend } from '@aws-amplify/backend';
 import {storage} from './storage/resource'
 
 export const backend = defineBackend({
-  storage,
+  // storage,
 });
 
 backend.addOutput({
   storage: {
-    // aws_region: "ap-southeast-1",
+    aws_region: "ap-southeast-1",
     bucket_name: "react-upload-file-with-url",
   }
 });
