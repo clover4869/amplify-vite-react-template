2219d9d22d4e82c1674bdeab8156734f57497f1f
clover4869 <153183368+clover4869@users.noreply.github.com>
2025-02-27 03:56:24 +0000

    feat: test

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,31 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
-import { storage } from './storage/resource';
+import { defineBackend, defineFunction } from '@aws-amplify/backend';
+import { Stack } from 'aws-cdk-lib';
+import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
 
-const backend =  defineBackend({
-  auth,
-  data,
-  storage
+const s3TriggerFunction = defineFunction({
+  name: 's3TriggerFunction',
+  entry: './storage/handle.ts',
 });
 
-const { cfnBucket: defaultBucket } = backend.storage.resources.cfnResources
-defaultBucket.bucketName = 'react-upload-file-with-url'
+export const backend = defineBackend({
+  s3TriggerFunction,
+});
+
+const { s3TriggerFunction: fn } = backend;
+const stack = Stack.of(fn);
+
+const existingBucket: IBucket = Bucket.fromBucketName(
+  stack,
+  'ExistingBucket',
+  'react-upload-file-with-url' // Replace with your bucket name
+);
+
+fn.addEventSourceMapping('S3Trigger', {
+  eventSourceArn: existingBucket.bucketArn,
+  eventSourceParams: {
+    s3: {
+      event: 's3:ObjectCreated:*',
+    },
+  },
+});
