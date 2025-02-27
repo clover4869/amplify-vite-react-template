ea92b49728f05c1683d09574e99da2734e2ffd16
clover4869 <153183368+clover4869@users.noreply.github.com>
2025-02-27 09:09:39 +0000

    test

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,5 @@
-import { defineBackend, defineFunction } from '@aws-amplify/backend';
-import { Stack } from 'aws-cdk-lib';
-import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
-import { Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
+import { defineBackend } from '@aws-amplify/backend';
 import {storage} from './storage/resource'
-
-const s3TriggerFunction = defineFunction({
-  name: 's3TriggerFunction',
-  entry: './storage/handle.ts',
-});
 
 export const backend = defineBackend({
   storage,
