5b55ca02a324ad4f55a0542ea3c1f4c703f54d8b
clover4869 <153183368+clover4869@users.noreply.github.com>
2025-02-28 10:25:37 +0000

    test

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 // import { SqsConsumerStack } from './cdk-custom/consumer-sqs/resource';
-// import { S3TriggerStack } from './cdk-custom/trigger-s3/resource';
+import { S3TriggerStack } from './cdk-custom/trigger-s3/resource';
 import { HelloWorldLambdaStack } from './cdk-custom/helloworld/resource';
 
 export const backend = defineBackend({
@@ -12,11 +12,11 @@ export const backend = defineBackend({
 //     'sqsConsumerStack',
 //     {}
 // );
-// new S3TriggerStack(
-//     backend.createStack('S3TriggerStack'),
-//     's3TriggerStack',
-//     {}
-// );
+new S3TriggerStack(
+    backend.createStack('S3TriggerStack'),
+    's3TriggerStack',
+    {}
+);
 
 new HelloWorldLambdaStack(
     backend.createStack('HelloWorldLambdaStack'),
--- amplify/cdk-custom/trigger-s3/resource.ts
+++ amplify/cdk-custom/trigger-s3/resource.ts
@@ -3,11 +3,11 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3Notifications from 'aws-cdk-lib/aws-s3-notifications';
 import { Construct } from 'constructs';
-import * as path from 'path';
+import { CfnOutput } from 'aws-cdk-lib';
 
 export class S3TriggerStack extends cdk.Stack {
   public readonly fileUploadBucket: s3.IBucket; // Use IBucket interface for an existing bucket
-  public readonly s3ProcessorLambda: lambda.Function;
+  // public readonly s3ProcessorLambda: lambda.Function;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -15,17 +15,35 @@ export class S3TriggerStack extends cdk.Stack {
     // Step 1: Use the existing S3 bucket
     this.fileUploadBucket = s3.Bucket.fromBucketName(this, 'FileUploadBucket', 'react-upload-file-with-url');
 
-    // Step 2: Create Lambda function to process S3 events
-    this.s3ProcessorLambda = new lambda.Function(this, 'S3ProcessorLambda', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'handler.handle',  // Lambda handler
-      code: lambda.Code.fromAsset('./amplify/cdk-custom/trigger-s3'), // Lambda code directory
+    // // Step 2: Create Lambda function to process S3 events
+    // this.s3ProcessorLambda = new lambda.Function(this, 'S3ProcessorLambda', {
+    //   runtime: lambda.Runtime.NODEJS_18_X,
+    //   handler: 'handler.handle',  // Lambda handler
+    //   code: lambda.Code.fromAsset('./amplify/cdk-custom/trigger-s3'), // Lambda code directory
+    // });
+
+    // Define the Lambda function
+    const s3ProcessorLambda = new lambda.Function(this, 'S3ProcessorLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X, // Specify the runtime
+      handler: 'handler.handler',           // Specify the handler function
+      code: lambda.Code.fromAsset('./amplify/cdk-custom/trigger-s3'),
+      functionName: 'S3ProcessorLambda',
+      description: 'This is my custom Lambda function created using CDK',
+      memorySize: 128,
     });
 
+
+
     // Step 3: Grant Lambda permissions to read from the S3 bucket
-    this.fileUploadBucket.grantRead(this.s3ProcessorLambda);
+    this.fileUploadBucket.grantRead(s3ProcessorLambda);
 
     // Step 4: Add S3 event notification to Lambda function
-    this.fileUploadBucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3Notifications.LambdaDestination(this.s3ProcessorLambda));
+    this.fileUploadBucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3Notifications.LambdaDestination(s3ProcessorLambda));
+
+    // Output the Lambda function ARN
+    new CfnOutput(this, 'S3ProcessorLambdaArn', {
+      value: s3ProcessorLambda.functionArn,
+      exportName: 'S3ProcessorLambdaArn',
+    });
   }
 }
