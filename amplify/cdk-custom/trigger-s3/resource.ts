7b76e02696540f3f47e3c1131c79f28c753b6631
clover4869 <153183368+clover4869@users.noreply.github.com>
2025-02-28 11:04:11 +0000

    test

--- amplify/cdk-custom/trigger-s3/resource.ts
+++ amplify/cdk-custom/trigger-s3/resource.ts
@@ -6,29 +6,16 @@ import { Construct } from 'constructs';
 import { CfnOutput } from 'aws-cdk-lib';
 
 export class S3TriggerStack extends cdk.Stack {
-  public readonly fileUploadBucket: s3.IBucket; // Use IBucket interface for an existing bucket
-  // public readonly s3ProcessorLambda: lambda.Function;
+  public readonly fileUploadBucket: s3.IBucket; 
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, {
-      ...props,
-      env: {
-        region: 'us-west-2',  // Replace with your region
-      }
+      ...props
     });
 
-    // Step 1: Use the existing S3 bucket
-    this.fileUploadBucket = s3.Bucket.fromBucketName(this, 'FileUploadBucket', 'react-upload-file-with-url');
+    this.fileUploadBucket = s3.Bucket.fromBucketName(this, 'FileUploadBucket', 'stk-upload-file');
 
-    // // Step 2: Create Lambda function to process S3 events
-    // this.s3ProcessorLambda = new lambda.Function(this, 'S3ProcessorLambda', {
-    //   runtime: lambda.Runtime.NODEJS_18_X,
-    //   handler: 'handler.handle',  // Lambda handler
-    //   code: lambda.Code.fromAsset('./amplify/cdk-custom/trigger-s3'), // Lambda code directory
-    // });
-    const region = 'ap-southeast-1'; 
 
-    // Define the Lambda function
     const s3ProcessorLambda = new lambda.Function(this, 'S3ProcessorLambda', {
       runtime: lambda.Runtime.NODEJS_18_X, // Specify the runtime
       handler: 'handler.handler',           // Specify the handler function
@@ -38,19 +25,13 @@ export class S3TriggerStack extends cdk.Stack {
       memorySize: 128,
       environment: {
         TEST: 'test',
-        AWS_REGION:'ap-southeast-1'
       }
     });
 
-
-
-    // Step 3: Grant Lambda permissions to read from the S3 bucket
     this.fileUploadBucket.grantRead(s3ProcessorLambda);
 
-    // Step 4: Add S3 event notification to Lambda function
     this.fileUploadBucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3Notifications.LambdaDestination(s3ProcessorLambda));
 
-    // Output the Lambda function ARN
     new CfnOutput(this, 'S3ProcessorLambdaArn', {
       value: s3ProcessorLambda.functionArn,
       exportName: 'S3ProcessorLambdaArn',
